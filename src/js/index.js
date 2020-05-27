8878b755d425b1d6e2c3f6bea106ae467107aaf0
Kevin James <zeddzorrander@gmail.com>
2020-05-27 18:07:11 -0400

    debugged so draw and erase methods work

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,31 @@
 //import './Geogebra/deployggb';
 //require("imports-loader?GGBApplet=GGBapplet!./Geogebra/deployggb.js");
 import functG from './functions/f.json';
-import GgbFunction from './models/GgbModel';
+import { GgbFunction } from './models/GgbModel';
 import { parameters, views } from './configGeo';
 import $ from 'jquery';
 
 let g = new GgbFunction(functG);
 g.applet = new GGBApplet(parameters, '5.0', views);
 
+function draw() {
+    g.draw();
+    //console.log(g.points.length);
+    document.getElementById('hidden').style.display = 'none';
+}
+
+function erase() {
+   // console.log(g.name);
+    g.erase();
+}
+
 window.onload = function() {
-    this.console.log(g);
+    console.log(g);
     g.applet.inject('ggbApplet', 'preferHTML5');
-    // $('#initGgb').click(function() {
-    //     g.draw();
-    // });
-    // window.document.getElementById('initGgb').click();
+    document.getElementById('initGgb').addEventListener('click', draw);
+    this.document.getElementById('erase').addEventListener('click', erase);
+    //$('#initGgb').click(draw);
+    //window.document.getElementById('initGgb').click();
 };
 
 g.applet.setPreviewImage('data:image/gif;base64,R0lGODlhAQABAAAAADs=','https://www.geogebra.org/images/GeoGebra_loading.png','https://www.geogebra.org/images/applet_play.png');
--- src/js/models/GgbModel.js
+++ src/js/models/GgbModel.js
@@ -1,6 +1,6 @@
 import '../Geogebra/deployggb';
 
-export default class GgbFunction {
+export class GgbFunction {
     constructor({name, formula, indVar = 'x', points = [], discontinuities = [], implicit = false, depVar}) {
         this.name = name;
         this.formula = formula;
@@ -12,14 +12,24 @@ export default class GgbFunction {
             this.depVar = depVar;
         } else this.depVar = this.name;
     }
+    
+    erase() {
+        ggbApplet.deleteObject(this.name);
+        if (this.points.length > 0) {
+            this.points.forEach(el => ggbApplet.deleteObject(el.name));
+        }
+    }
+
     draw() {
-        ggbApplet.evalCommand(this.formula);
-        ggbApplet.setColor(this.name,0,0,0);
-        ggbApplet.setLineThickness(this.name,7);
+        if (!this.implicit) {
+            ggbApplet.evalCommand(`${this.name} = ${this.formula}`);
+        } else ggbApplet.evalCommand(this.formula);
+        ggbApplet.setColor(this.name, 0, 0 ,0);
+        ggbApplet.setLineThickness(this.name, 7);
         ggbApplet.setCaption(this.name, `$${this.name}(x)$`);
         ggbApplet.setLabelStyle(this.name, 3);
         ggbApplet.setLabelVisible(this.name, true);
-        ggbApplet.setAxisLabels(1, `$${this.indVar}$`, `$${this.depVar}$`);
+        ggbApplet.setAxisLabels(1, `$${this.indVar}$`, `${this.depVar}`);
         if (this.points.length > 0) {
             this.points.forEach(el => {
                 ggbApplet.evalCommand(`${el.name} = (${el.x}, ${el.y})`);
@@ -29,10 +39,22 @@ export default class GgbFunction {
             });
         }
     }
-    erase() {
-        ggbApplet.deleteObject(this.name);
-        if (this.points.length > 0) {
-            this.points.forEach(el => ggbApplet.deleteObject(el.name));
-        }
-    }
 }
+
+// GgbFunction.prototype.draw = function() {
+//     ggbApplet.evalCommand(this.formula);
+//     ggbApplet.setColor(this.name,0,0,0);
+//     ggbApplet.setLineThickness(this.name,7);
+//     ggbApplet.setCaption(this.name, `$${this.name}(x)$`);
+//     ggbApplet.setLabelStyle(this.name, 3);
+//     ggbApplet.setLabelVisible(this.name, true);
+//     ggbApplet.setAxisLabels(1, `$${this.indVar}$`, `$${this.depVar}$`);
+//     if (this.points.length > 0) {
+//         this.points.forEach(el => {
+//             ggbApplet.evalCommand(`${el.name} = (${el.x}, ${el.y})`);
+//             ggbApplet.setPointStyle(el.name, el.style);
+//             ggbApplet.setPointSize(el.name, el.size);
+//             ggbApplet.setColor(el.name, el.colorR, el.colorG, el.colorB);
+//         });
+//     }
+// }
