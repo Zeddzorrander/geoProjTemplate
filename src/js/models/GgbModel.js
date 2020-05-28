616a7c03a1382bb93d996f7fa7f0d2b9eaf467ae
Kevin James <zeddzorrander@gmail.com>
2020-05-28 13:49:04 -0400

    added color and size as properties to functions

--- src/js/models/GgbModel.js
+++ src/js/models/GgbModel.js
@@ -1,13 +1,17 @@
 import '../Geogebra/deployggb';
 
 export class GgbFunction {
-    constructor({name, formula, indVar = 'x', points = [], discontinuities = [], implicit = false, depVar}) {
+    constructor({name, formula, colorR, colorB, colorG, size, indVar = 'x', points = [], discontinuities = [], implicit = false, depVar}) {
         this.name = name;
         this.formula = formula;
         this.points = points;
         this.discontinuities = discontinuities;
         this.indVar = indVar;
         this.implicit = implicit;
+        this.colorR = colorR;
+        this.colorG = colorG;
+        this.colorB = colorB;
+        this.size = size;
         if (implicit) {
             this.depVar = depVar;
         } else this.depVar = this.name;
@@ -24,8 +28,8 @@ export class GgbFunction {
         if (!this.implicit) {
             ggbApplet.evalCommand(`${this.name} = ${this.formula}`);
         } else ggbApplet.evalCommand(this.formula);
-        ggbApplet.setColor(this.name, 0, 0 ,0);
-        ggbApplet.setLineThickness(this.name, 7);
+        ggbApplet.setColor(this.name, this.colorR, this.colorG ,this.colorB);
+        ggbApplet.setLineThickness(this.name, this.size);
         ggbApplet.setCaption(this.name, `$${this.name}(x)$`);
         ggbApplet.setLabelStyle(this.name, 3);
         ggbApplet.setLabelVisible(this.name, true);
