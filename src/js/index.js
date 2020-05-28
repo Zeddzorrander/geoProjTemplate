a0b8cc44807c84702a5911645b705c7f5cfd1e11
Kevin James <zeddzorrander@gmail.com>
2020-05-28 15:51:15 -0400

    added relMax and relMin methods to GgbModel.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import './Geogebra/deployggb';
 //require("imports-loader?GGBApplet=GGBapplet!./Geogebra/deployggb.js");
-import functG from './functions/f.json';
+import functF from './functions/f.json'
+import functG from './functions/g.json';
 import imp from './functions/implicit.json';
 import { GgbFunction } from './models/GgbModel';
 import { parameters, views } from './configGeo';
@@ -10,14 +11,16 @@ import $ from 'jquery';
 
 
 
-
+let f = new GgbFunction(functF);
 let g = new GgbFunction(functG);
 let implicit = new GgbFunction(imp);
+f.applet = new GGBApplet(parameters, '5.0', views);
 g.applet = new GGBApplet(parameters, '5.0', views);
 implicit.applet = new GGBApplet(parameters, '5.0', views);
 
 function draw() {
-    g.draw();
+    f.draw();
+    console.log(f.relMax(-3, -1));
     // implicit.draw();
     //console.log(g.points.length);
     //document.getElementById('hidden').style.display = 'none';
@@ -31,6 +34,7 @@ function erase() {
 
 window.onload = function() {
     console.log(g);
+    f.applet.inject('ggbApplet', 'preferHTML5');
     g.applet.inject('ggbApplet', 'preferHTML5');
     implicit.applet.inject('ggbApplet', 'preferHTML5');
     document.getElementById('initGgb').addEventListener('click', draw);
--- src/js/models/GgbModel.js
+++ src/js/models/GgbModel.js
@@ -50,22 +50,54 @@ export class GgbFunction {
             });
         }
     }
-}
 
-// GgbFunction.prototype.draw = function() {
-//     ggbApplet.evalCommand(this.formula);
-//     ggbApplet.setColor(this.name,0,0,0);
-//     ggbApplet.setLineThickness(this.name,7);
-//     ggbApplet.setCaption(this.name, `$${this.name}(x)$`);
-//     ggbApplet.setLabelStyle(this.name, 3);
-//     ggbApplet.setLabelVisible(this.name, true);
-//     ggbApplet.setAxisLabels(1, `$${this.indVar}$`, `$${this.depVar}$`);
-//     if (this.points.length > 0) {
-//         this.points.forEach(el => {
-//             ggbApplet.evalCommand(`${el.name} = (${el.x}, ${el.y})`);
-//             ggbApplet.setPointStyle(el.name, el.style);
-//             ggbApplet.setPointSize(el.name, el.size);
-//             ggbApplet.setColor(el.name, el.colorR, el.colorG, el.colorB);
-//         });
-//     }
-// }
+    evaluate(x) {
+        if (this.discontinuities.length > 0) {
+            this.discontinuities.forEach(el => {
+                if (el.x == x) return el.value;
+            });
+        } else {
+            return ggbApplet.getValue(`${this.name}(${x})`);
+        }
+    }
+
+    // if function is monotonic on (leftEndpt, rightEndPt), this will return an endpoint.  Otherwise, it returns the relMax
+    relMax(leftEndpt, rightEndpt) {
+        const relMaxPt = this.getRelExtreme(leftEndpt, rightEndpt, 'Max');
+        if (this.discontinuities.length > 0) {
+            this.discontinuities.forEach(el => {
+                if (el.x == relMaxPt.relMaxLoc) {
+                   alert('There is a discontinuity!  Please try a different interval');
+                }
+            });
+        } else return relMaxPt.relMax;
+    }
+
+    // if function is monotonic on (leftEndPt, rightEndPt), this will return an endpoint.  Otherwise returns the relMin
+    relMin(leftEndPt, rightEndPt) {
+        const relMinPt = this.getRelExtreme(leftEndPt, rightEndPt, 'Min');
+        if (this.discontinuities.length > 0) {
+            this.discontinuities.forEach(el => {
+                if (el.x == relMinPt.relMinLoc) {
+                   alert('There is a discontinuity!  Please try a different interval');
+                }
+            });
+        } else return relMinPt.relMin;
+    }
+    
+    // type is 'Max' or 'Min'.  This is an internal helper function
+    getRelExtreme(leftEndpt, rightEndpt, type) {
+        ggbApplet.evalCommand(`rel${type} = ${type}(${this.name},${leftEndpt},${rightEndpt})`);
+        ggbApplet.setVisible(`rel${type}`, false);
+        if (type === 'Max') {
+            const relMax = Math.round(100000 * parseFloat(ggbApplet.getYcoord('relMax'))) / 100000;
+            const relMaxLoc = Math.round(100000 * parseFloat(ggbApplet.getXcoord('relMax'))) / 100000;
+            return { relMaxLoc, relMax };
+        } else {
+            const relMin = Math.round(100000 * parseFloat(ggbApplet.getYcoord('relMax'))) / 100000;
+            const relMinLoc = Math.round(100000 * parseFloat(ggbApplet.getXcoord('relMax'))) / 100000;
+            return { relMinLoc, relMin };
+            
+        }
+    }
+}
